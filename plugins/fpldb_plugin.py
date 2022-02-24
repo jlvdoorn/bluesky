c3b2ef43c0537414a4772fb25335ae032887c28f
Jan van Doorn <jlvdoorn@outlook.com>
2022-02-24 13:35:01 +0100

    updated, fixed auto deletion, added documentation

--- plugins/fpldb_plugin.py
+++ plugins/fpldb_plugin.py
@@ -3,11 +3,12 @@ based on a user input for point of departure and arrival using Flight Plan Datab
 It then compares the points in this routes with the existing nav database in bluesky.
 If the points are not already present, it will add them to the database."""
 
-###################### TODO ####################
-# Implement SID/STAR                           #
-# Fix CheckDB?!                                #
-# Add airways?                                 #
-################################################
+###################### TODO ###########################
+# Implement SID/STAR - Is that even possible?!        #
+# Fix CheckDB?!                                       #
+# Add airways? - Needed? Route points form airway     #
+# Add auto return flight after time interval - Option #
+#######################################################
 
 
 # Import the global bluesky objects. Uncomment the ones you need
@@ -17,6 +18,11 @@ import requests, json
 ### Initialization function of your plugin. Do not change the name of this
 ### function, as it is the way BlueSky recognises this file as a plugin.
 def init_plugin():
+    """
+    An initiation function of the plugin. 
+    This function is called when the plugin is loaded.
+    It gives defines the type and the name of the plugin.
+    """
 
     # Addtional initilisation code
     
@@ -34,6 +40,22 @@ def init_plugin():
 
 class Route():  
     def __init__(self,acid: str,fromICAO: str = "EHAM",toICAO: str = "LFMN"):
+        """
+        The init function of the class Route. This function is called when a new route 
+        is created using this class. It sets parameters as the:
+        - apiUrl and apiKey
+        - acid
+        - fromICAO
+        - toICAO
+
+        Parameters
+        ----------
+        acid : str
+        fromICAO : str, optional
+            The origin of the aircraft. Default: EHAM
+        toICAO : str, optional
+            The destination of the aircraft. Default: LFMN
+        """
         self.apiUrl = "https://api.flightplandatabase.com/"
         self.apiKey = "1328fIdYaRVo8Yx5nJiw3jtFB2O2L5sE0OVGX7Md"
         self.jsonFormat = "application/json"
@@ -43,7 +65,11 @@ class Route():
         self.toICAO = toICAO
 
     def generate_fltplan(self):
-        ## Generate flightplan
+        """
+        This function creates a flight plan with the use of FlightPlanDatabase.
+        The aircraft id, origin and destination are used that are defined when calling the FPLR command.
+        """
+
         genUrl = "https://api.flightplandatabase.com/auto/generate"
         data = {"fromICAO":self.fromICAO,"toICAO":self.toICAO}
         headers = {"Content-Type":self.jsonFormat,"Units":"AVIATION"}
@@ -61,7 +87,10 @@ class Route():
         self.encodedPolyline = jR["encodedPolyline"]
 
     def download_fltplan_data(self):
-        ## Download flightplan data
+        """
+        This function downloads the data of the flightplan created. It then implements this data into the object itself.
+        """
+
         dataUrl = "https://api.flightplandatabase.com/plan/"
         data = {"id":int(self.id)}
         headers = {"Content-Type":self.fplFormat,"Units":"METRIC"}
@@ -95,7 +124,17 @@ class Route():
         self.nodeAlt = nodeAlt
         self.nodeVia = nodeVia
             
-    def checkDB(self): # Checks navdb and adds points if they are missing
+    def checkDB(self):
+        """
+        This function checks the navdb of BlueSky. If any of the points in the downloaded flightplan
+        is not yet in the navdb, then it adds them to the navdb permanently. 
+        
+
+        WARNING
+        -------
+        CURRRENTLY IT GIVES AN OVERFLOW ERROR MESSAGE AND BLUESKY WILL CRASH - WORK IN PROGRESS
+        """
+
         for node in range(len(self.nodeIdent)):
             for wpt in navdb.wpid:
                 if wpt == self.nodeIdent[node]:
@@ -109,6 +148,11 @@ class Route():
                         pass
 
     def addToDB(self):
+        """
+        This function adds all the points of the downloaded flightplan to the current user database.
+        The database is used only for the current simulation, so the points are removed after the simulation ends.
+        """
+
         for node in range(len(self.nodeIdent)):
             if self.nodeType[node] == 'VOR' or self.nodeType[node] == 'NDB' or self.nodeType[node] == 'ILS' or self.nodeType[node] == 'LOC' or self.nodeType[node] == 'GS' or self.nodeType[node] == 'OM' or self.nodeType[node] == 'MM' or self.nodeType[node] == 'IM' or self.nodeType[node] == 'DME' or self.nodeType[node] == 'TACAN' or self.nodeType[node] == 'FIX':
                 navdb.defwpt(self.nodeIdent[node],self.nodeLat[node],self.nodeLon[node],self.nodeType[node])
@@ -116,21 +160,32 @@ class Route():
                 pass
 
     def addWPT(self): # Adds all waypoints to route
-        for node in range(len(self.nodeIdent)):
-            stack.stack("ORIG "+self.acid+" "+self.fromICAO)
+        """
+        This function adds all the points in the downloaded flightplan to the current route of an aircraft. 
+        The route then ceated is a 3D trajectory.
+        It also automatically deletes the flight when it reaches it destination.
+        """
+
+        stack.stack("ORIG "+self.acid+" "+self.fromICAO) # Add origin
+
+        for node in range(len(self.nodeIdent)): # Add all waypoints           
             stack.stack("ADDWPT "+self.acid+" "+self.nodeIdent[node]+" "+str(self.nodeAlt[node]))
-            stack.stack("DEST "+self.acid+" "+self.toICAO)
+        stack.stack("DEST "+self.acid+" "+self.toICAO) # Add destination
+        stack.stack(self.acid+" AT "+self.toICAO+" DO DEL "+self.acid) # Remove flight when at destination
 
 @stack.command(name='FPLR')
 def createRoute(acid: str,fromICAO: str = "EHAM",toICAO: str = "LFMN"):
-    ''''
+    """
     This command will create a route from a starting ICAO to arriving ICAO using flight plan database.
-    Furthermore it will check if the route points are already in the nav database of bluesky and will add them if they are not.
+    Furthermore it will check if the route points are already in the navdb of bluesky and will add them if they are not.
 
-    Arguments:
-    - fromICAO: airport of departure in ICAO format
-    - toICAO: airport of arrival in ICAO format
-    '''
+    Parameters
+    ----------
+    fromICAO: str
+        airport of departure in ICAO format
+    toICAO: str
+        airport of arrival in ICAO format
+    """
     r = Route(acid,fromICAO, toICAO)
     r.generate_fltplan()
     r.download_fltplan_data()
